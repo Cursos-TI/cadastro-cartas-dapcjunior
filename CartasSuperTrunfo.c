60d8df3744fd5a09a07da3ea4f94ca88b1075faa
Daniel Pereira <danielpereiracamposjunior@gmail.com>
2026-04-01 21:44:48 -0300

    inicio do  sesafio
    Aceitei o  desafio do  super trunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -4,12 +4,14 @@
 // Tema 1 - Cadastro das cartas
 // Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
 
-int main() {
+int main() 
+{
+  printf("")
   // Área para definição das variáveis para armazenar as propriedades das cidades
 
   // Área para entrada de dados
 
   // Área para exibição dos dados da cidade
 
-return 0;
+  return 0;
 } 
